553a4ac01a6226e14d790a5f87e91a135b5c7b00
PunnBoi <156408453+PunnBoi@users.noreply.github.com>
2024-03-01 09:43:57 +0000

    for mark

--- main.c
+++ main.c
@@ -21,7 +21,7 @@ for ( i = 1; i < argc; ++i ) {
    inOrder( b.root );
 
    // traverse the tree postOrder
- printf("\npostOrder:\n" );   postOrder( b.root );
+ printf("\npostOrder\n" );   postOrder( b.root );
 
     printf("\n");
 
